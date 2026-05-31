45d4d5a39f24f5fbbcbeddb9bc34c0214b17d997
ghoufranedaikra-bit <ghoufrane.daikra@univ-constantine2.dz>
2026-05-31 21:36:33 +0100

    fix static debug

--- backend/settings.py
+++ backend/settings.py
@@ -80,7 +80,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
+DEBUG = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
